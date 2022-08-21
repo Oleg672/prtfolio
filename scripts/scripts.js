ebe5a5de4a251377e3840320cb96dc77b8962003
Oleg672 <oleg6080826@mail.ru>
2022-08-21 18:03:53 +0300

    add main

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+function showNav() {
+
+    var b = document.getElementsByClassName("header__nav");
+    b[0].id = "show";
+    var c = document.getElementsByClassName("header__nav-mini");
+    c[0].id = ('');
+    var a = document.getElementsByClassName("header__nav-close");
+    a[0].id = "show";
+    var d = document.getElementsByClassName("logo");
+    d[0].id = ('');
+}
+function closeNav() {
+
+    var b = document.getElementsByClassName("header__nav");
+    b[0].id = "";
+    var a = document.getElementsByClassName("header__nav-close");
+    a[0].id = "";
+    var c = document.getElementsByClassName("header__nav-mini");
+    c[0].id = ('show');
+    var d = document.getElementsByClassName("logo");
+    d[0].id = ('show');
+}
+
