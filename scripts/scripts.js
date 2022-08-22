f61f84f807305f5381b4def3cd19f2d0031b784e
Oleg672 <oleg6080826@mail.ru>
2022-08-22 11:55:12 +0300

    add media

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,41 @@
+const images = document.querySelectorAll(".projects__item img");
+const sliderLine = document.querySelector(".projects__list");
+let count = 0;
+let width;
+function init() {
+
+    width = document.querySelector(".projects__carusel").offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollslider();
+}
+
+window.addEventListener('resize', init)
+init();
+
+document.querySelector('.btn-right').addEventListener('click', function () {
+    count++;
+    if (count >= images.length) { count = 0 }
+    rollslider();
+})
+document.querySelector('.btn-left').addEventListener('click', function () {
+    count--;
+    if (count < 0) { count = images.length - 1 }
+    rollslider();
+})
+function rollslider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+    console.log(count);
+}
+
+
+
+
+
+
 function showNav() {
 
     var b = document.getElementsByClassName("header__nav");
@@ -20,4 +58,10 @@ function closeNav() {
     var d = document.getElementsByClassName("logo");
     d[0].id = ('show');
 }
+
+
+
+
+
+
 
