ef312106f015bb538dd47ef3e20d0ab58feb3076
Oleg672 <oleg6080826@mail.ru>
2022-08-28 16:45:46 +0300

    add main

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,47 @@
 const images = document.querySelectorAll(".projects__item img");
+let widthItem = document.querySelector(".projects__item").offsetWidth;
 const sliderLine = document.querySelector(".projects__list");
 let count = 0;
 let width;
 function init() {
 
     width = document.querySelector(".projects__carusel").offsetWidth;
-    sliderLine.style.width = width * images.length + 'px';
-    images.forEach(item => {
-        item.style.width = width + 'px';
-        item.style.height = 'auto';
-    });
-    rollslider();
+    if (width <= 425) {
+        console.log(width);
+        sliderLine.style.width = width * images.length + 'px';
+        images.forEach(item => {
+            item.style.width = width + 'px';
+            item.style.height = 'auto';
+        });
+        rollslider();
+    }
+
 }
 
-    window.addEventListener('resize', init)
-    init();
+window.addEventListener('resize', init)
+init();
 
 
 document.querySelector('.btn-right').addEventListener('click', function () {
     count++;
     if (count >= images.length) { count = 0 }
+    console.log(width);
     rollslider();
 })
 document.querySelector('.btn-left').addEventListener('click', function () {
     count--;
     if (count < 0) { count = images.length - 1 }
+    console.log(width);
     rollslider();
 })
 function rollslider() {
-    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
-    console.log(count);
+    if (width >= 425) {
+
+        sliderLine.style.transform = 'translate(-' + count * widthItem + 'px)';
+    } else {
+
+        sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+    }
 }
 
 
@@ -47,6 +59,10 @@ function showNav() {
     a[0].id = "show";
     var d = document.getElementsByClassName("logo");
     d[0].id = ('');
+    var e = document.getElementsByClassName("wrapper__max-width");
+    e[0].id='nav-full-screen';
+    
+
 }
 function closeNav() {
 
@@ -58,6 +74,8 @@ function closeNav() {
     c[0].id = ('show');
     var d = document.getElementsByClassName("logo");
     d[0].id = ('show');
+    var e = document.getElementsByClassName("wrapper__max-width");
+    e[0].id='';
 }
 
 
